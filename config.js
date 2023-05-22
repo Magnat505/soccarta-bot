2351a761cff66d8b4171063ff5a9dc13e1a3b9ae
Magnat505 <83225374+Magnat505@users.noreply.github.com>
2023-05-22 17:36:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    token: process.env.TOKEN,
-    port: process.env.PORT,
-    db: process.env.DB,
-    admin: process.env.ADMIN
+    token: "1780915784:AAG_ODGdaFlM9QePZTAODnFX0f2dZ_fs08o",
+    port: 80,
+    db: "mongodb+srv://aki_kh:akbarshoh@cluster0.k6xvr.mongodb.net/soccarta-bot?retryWrites=true&w=majority",
+    admin: 986787968
 }
+
