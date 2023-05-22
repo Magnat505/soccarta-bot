57d4185a2047f2fcdd5942aafee4822dc9eb9924
Magnat505 <83225374+Magnat505@users.noreply.github.com>
2023-05-22 21:19:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  "ADMIN": "986787968",
-  "BTN_FOUR": "https://t.me/kartasoc",
-  "BTN_ONE": "https://telegra.ph/PREIMUSHCHESTVA-SOCIALNOJ-KARTY-04-04",
-  "BTN_THREE": "https://t.me/KartaSocMsk",
-  "DB": "mongodb+srv://aki_kh:akbarshoh@cluster0.k6xvr.mongodb.net/soccarta-bot?retryWrites=true&w=majority",
-  "PORT": "80",
-  "TOKEN": "1112783569:AAG-Ub-FrtztV_v1szXBeOYcTnYXnN0VxUw"
+    token: process.env.TOKEN,
+    port: process.env.PORT,
+    db: process.env.DB,
+    admin: process.env.ADMIN
 }
