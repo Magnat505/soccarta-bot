ed4cb64f3200d4a9ac8f2eb6baae20965de242f6
Magnat505 <83225374+Magnat505@users.noreply.github.com>
2023-05-22 16:59:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: process.env.TOKEN,
+    token: 1112783569:AAG-Ub-FrtztV_v1szXBeOYcTnYXnN0VxUw,
     port: process.env.PORT,
     db: process.env.DB,
     admin: process.env.ADMIN
