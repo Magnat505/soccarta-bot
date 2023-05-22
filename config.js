e5325c80db6d48bf773d2fb066e86a24f8ae6b61
Magnat505 <83225374+Magnat505@users.noreply.github.com>
2023-05-22 17:44:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    token: "1780915784:AAG_ODGdaFlM9QePZTAODnFX0f2dZ_fs08o",
-    port: 80,
-    db: "mongodb+srv://aki_kh:akbarshoh@cluster0.k6xvr.mongodb.net/soccarta-bot?retryWrites=true&w=majority",
-    admin: 986787968
+  "ADMIN": "986787968",
+  "BTN_FOUR": "https://t.me/kartasoc",
+  "BTN_ONE": "https://telegra.ph/PREIMUSHCHESTVA-SOCIALNOJ-KARTY-04-04",
+  "BTN_THREE": "https://t.me/KartaSocMsk",
+  "DB": "mongodb+srv://aki_kh:akbarshoh@cluster0.k6xvr.mongodb.net/soccarta-bot?retryWrites=true&w=majority",
+  "PORT": "80",
+  "TOKEN": "1112783569:AAG-Ub-FrtztV_v1szXBeOYcTnYXnN0VxUw"
 }
-
